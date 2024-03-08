d8bacd602d18829fad9d9117317689751541d8b4
bohdanbtw <mybuisnessbohdan@gmail.com>
2024-03-08 22:16:03 +0200

    Lab01 edited

--- Lab01/mymain.py
+++ Lab01/mymain.py
@@ -1,1 +1,10 @@
 print("Hello world!")
+import numpy as pd
+import matplotlib.pyplot as plt
+
+array = pd.random.rand(100)
+
+plt.plot(array)
+plt.xlabel("Індекс")
+plt.ylabel("Значення")
+plt.show()
